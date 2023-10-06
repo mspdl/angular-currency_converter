1d202c78df2831a916bdbba5ea2b29277b013d1b
Morgan <morgan.espindola@gmail.com>
2023-10-05 23:19:46 -0300

    Converter Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { ConverterModule } from './converter/converter.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, ConverterModule],
   providers: [],
   bootstrap: [AppComponent],
 })

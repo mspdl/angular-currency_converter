c5f9aaf67bd254d86d694db9eaa6e7038a67e32a
Morgan <morgan.espindola@gmail.com>
2023-10-06 22:14:56 -0300

    Created Converter Component

--- src/app/converter/converter.module.ts
+++ src/app/converter/converter.module.ts
@@ -1,12 +1,10 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { NgModule } from '@angular/core';
+import { ConverterComponent } from './components/converter.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [ConverterComponent],
+  imports: [CommonModule],
+  exports: [ConverterComponent],
 })
-export class ConverterModule { }
+export class ConverterModule {}

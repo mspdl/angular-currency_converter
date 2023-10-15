dc0a8e26825912ca149d709e90208e7283c4ce47
Morgan <morgan.espindola@gmail.com>
2023-10-15 19:19:25 -0300

    Added ng value accessor

--- src/app/converter/directives/number.directive.ts
+++ src/app/converter/directives/number.directive.ts
@@ -1,10 +1,17 @@
-import { Directive, HostListener } from '@angular/core';
+import { Directive, ElementRef, HostListener } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Directive({
   selector: '[number]',
+  providers: [
+    { provide: NG_VALUE_ACCESSOR, useExisting: NumberDirective, multi: true },
+  ],
 })
-export class NumberDirective {
-  constructor() {}
+export class NumberDirective implements ControlValueAccessor {
+  onTouched: any;
+  onChanged: any;
+
+  constructor(private el: ElementRef) {}
 
   @HostListener('keyup', ['$event'])
   onKeyUp($event: any) {
@@ -17,5 +24,16 @@ export class NumberDirective {
       value = value.substr(0, decimalsPos) + '.' + value.substr(decimalsPos);
     }
     $event.target.value = value;
+    this.onChanged(value);
+  }
+
+  writeValue(newValue: any): void {
+    this.el.nativeElement.value = newValue;
+  }
+  registerOnChange(fn: any): void {
+    this.onChanged = fn;
+  }
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
   }
 }

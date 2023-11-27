01a53dd709518d38e8d83a27425a4b4926ba2eba
Morgan <morgan.espindola@gmail.com>
2023-11-27 20:13:30 -0300

    Exchange Rate Modal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ConverterModule } from './converter/converter.module';
+import { ExchangeRateComponent } from './converter/utils/exchange-rate.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ExchangeRateComponent],
   imports: [BrowserModule, ConverterModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/converter/utils/exchange-rate.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-exchange-rate',
+  templateUrl: './exchange-rate.component.html',
+  styleUrls: ['./exchange-rate.component.scss']
+})
+export class ExchangeRateComponent {
+
+}
